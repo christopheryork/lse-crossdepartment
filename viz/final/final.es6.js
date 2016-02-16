63d412e89f0f9230ab0749d972630175408b60c8
Christopher W York <christopher.w.york@gmail.com>
2016-02-16 12:22:55 +0000

    Dual with mouseover and transitions in final

--- viz/final/final.es6.js
+++ viz/final/final.es6.js
@@ -1,10 +1,27 @@
+//
+// Cross-Departmental Research and Teaching DV
+//
+// (c) Christopher York 2016
+//     Communications
+//     London School of Economics
+//
+
 // This file must be pre-processed for Safari, as it uses arrow functions.
+
+// TODO
+//   - chord colors during selection should be by *opposite* dept color
+//   - relayout on resize of window
+//   - each view in a separate group, fade in on select
+//   - move through modes on a timer                                      DONE
+//   - shouldn't advance modes during hover on a department
+//   - faculty sorting for dual chord
 
 queue().defer(d3.csv, "../data-6.1,6.3.csv")
        .defer(d3.csv, "../data-6.2.csv")
        .defer(d3.csv, "../data-6.4.csv")
        .await( (err, depts, research, teaching) => {
   if(err) { throw err }
+
 
   // convert data from JSON to javascript types
   depts.forEach( (d) => { d.faculty = +d.faculty; d.research_links = +d.research_links; d.teaching_links = +d.teaching_links })
@@ -35,22 +52,228 @@ queue().defer(d3.csv, "../data-6.1,6.3.csv")
     return m
   }
 
-  var research_matrix = populate(research, empty_matrix(n))
-  var teaching_matrix = populate(teaching, empty_matrix(n))
+  var sum = (vector) => vector.reduce( (a,b) => a+b, 0.0)
+  var matrix_add = lift( (a,b) => a+b )
+  var matrix_subtract = lift( (a,b) => a-b )
+
+  var research_matrix = populate(research, constant_matrix(n))
+  var teaching_matrix = populate(teaching, constant_matrix(n))
 
 
   // vizualization proper
 
-  var width, height,
-      innerRadius, outerRadius, chordRadius, labelRadius
+  var width = window.innerWidth,
+      height = width * 0.7,
+      innerRadius = width * .175,
+      outerRadius = innerRadius * 1.05,
+      chordRadius = innerRadius * 0.99,
+      labelRadius = innerRadius * 1.15,
+      padAngle = 0.01,
+      chordWidth = 0.04,
+      mode_dur = 750,
+      firstSlide = 2500,
+      slideSpeed = 7500
 
   var margins = { top: 0, left: 150, right: 50, bottom: 0 }
 
   var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-  var g = svg.append("g")
-        .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
+  var fill = d3.scale.category20c()
+    .domain(d3.range(0, n))
 
+  var research_g = svg.append("g")
+        .attr("transform", "translate(" + (margins.left + labelRadius) + "," + (margins.top + labelRadius) + ")")
+
+  var teaching_g = svg.append("g")
+        .attr("transform", "translate(" + (width - margins.right - labelRadius) + "," + (margins.top + labelRadius) + ")")
+
+  var arc = d3.svg.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
+
+  var label_arc = d3.svg.arc()
+    .innerRadius(labelRadius)
+    .outerRadius(outerRadius)
+
+  var chord = d3.svg.chord()
+    .radius(chordRadius)
+
+  function arc_center(d, width) {
+    width = width || 0.1
+    var c = d3.mean([d.startAngle, d.endAngle]),
+        s = d3.max([d.startAngle, c - width])
+        t = d3.min([c + width, d.endAngle])
+
+    return { startAngle: s, endAngle: t }
+  }
+
+  function calc_links(data, node_positions) {
+    var links = []
+    var i=-1; while(i++<n-1) {
+      var j=-1; while(j++<i-1) {
+        var val = data[i][j]
+        if(val > 0) {
+          links.push({ source: arc_center(node_positions[i], chordWidth),
+                       target: arc_center(node_positions[j], chordWidth),
+                       value: val,
+                       source_index: i,
+                       target_index: j})
+        }
+      }
+    }
+    return links
+  }
+
+  var pie = d3.layout.pie()
+    .padAngle(padAngle)
+    .value(sum)
+
+  var sortsum = (a,b) => d3.descending(sum(a),sum(b))
+
+  var pies = {
+    department: pie.sort(null)(constant_matrix(n, 1)),
+    faculty:    pie.sort(sortsum)(constant_matrix(n, 1)),
+    links:      pie.sort(sortsum)( matrix_add(research_matrix, teaching_matrix) ),
+    emphasis:   pie.sort(sortsum)( matrix_subtract(research_matrix, teaching_matrix) )
+  }
+
+  function update_chord(g, data, node_positions) {
+
+    // update layout
+    var link_info = calc_links(data, node_positions)
+
+    // transition nodes
+
+    var node = g.selectAll(".dept")
+        .data(node_positions)
+
+    node.exit().remove()
+
+    var node_g = node.enter().append("g")
+        .attr("class", (d,i) => "dept dept_" + i)
+
+    node_g.append("path")
+      .attr("fill", (d,i) => fill(i))
+
+    node_g.append("text")
+      .attr("opacity", 0)
+
+    var trans = node.transition()
+      .duration(mode_dur)
+
+    trans.select("path")
+        .attrTween("d", function(d) { // "this" below requires function...
+          var interp = d3.interpolate(this._current || d, d)
+          this._current = d
+          return (t) => arc(interp(t))
+        })
+
+    trans.select("text")
+      .attr("transform", (d) => "translate(" + label_arc.centroid(d) + ")" )
+      .attr("text-anchor", (d) => arc_center(d, chordWidth).startAngle < Math.PI ? "start" : "end")
+      .text( (d, i) => dept_names[i])
+
+    // transition links
+
+    var link = g.selectAll(".link")
+        .data(link_info, (d) => [d.source_index, d.target_index].join("x"))
+
+    link.exit()
+      .transition()
+        .duration(mode_dur)
+        .attr("opacity", 0)
+      .remove()
+
+    link.enter()
+      .append("path")
+        .attr("class", "link")
+        .attr("fill", (d) => fill(d3.min([d.source_index, d.target_index])))
+        .attr("opacity", 0)
+
+    link.transition()
+      .duration(mode_dur)
+        .attr("opacity", 1)
+        .attrTween("d", function(d) { // "this" below requires function...
+          var interp = d3.interpolate(this._current || d, d)
+          this._current = d
+          return (t) => chord(interp(t))
+        })
+  }
+
+  function listeners(elems) {
+
+    elems.forEach( (g) => {
+      g.selectAll(".dept")
+        .on("mouseenter", focus)
+        .on("mouseout", defocus)
+    })
+
+    function focus(d0,i0) {
+      elems.forEach( (g) => {
+
+        // collect list of linked departments
+        var affiliated = d3.set()
+        g.selectAll(".link")
+          .filter( (d) => d.source_index === i0 || d.target_index === i0 )
+          .each( (d) => { affiliated.add(d.source_index); affiliated.add(d.target_index) })
+
+        // transition graph
+        var trans = g.transition()
+        trans.selectAll(".dept text")
+          .attr("opacity", (d,i) => affiliated.has(i) || i === i0 ? 1 : 0)
+        trans.selectAll(".link")
+          .attr("opacity", (d,i) => d.source_index === i0 || d.target_index === i0 ? 1 : 0.05)
+
+      })
+    }
+
+    function defocus() {
+      elems.forEach( (g) => {
+        var trans = g.transition()
+        trans.selectAll(".link")
+          .attr("opacity", 1)
+        trans.selectAll(".dept text")
+          .attr("opacity", 0)
+      })
+    }
+  }
+
+  research_g.call(update_chord, research_matrix, pies.department)
+  teaching_g.call(update_chord, teaching_matrix, pies.department)
+
+  listeners([research_g, teaching_g])
+
+  // start off moving into alphabetical order
+
+  function order(key) {
+    research_g.call(update_chord, research_matrix, pies[key])
+    teaching_g.call(update_chord, teaching_matrix, pies[key])
+  }
+
+  var timeout = setTimeout(advance, firstSlide)
+
+  function advance() {
+    var keys = []
+    d3.selectAll('#order input[type="radio"]').each( function() { keys.push(this.value) } )
+
+    var val = d3.select('#order :checked').node().value
+    var nextval = keys[ (keys.indexOf(val) + 1) % keys.length ]
+
+    order(nextval)
+    d3.select('#order input[value="' + nextval + '"]').property('checked', true)
+
+    timeout = setTimeout(advance, slideSpeed)
+  }
+
+  d3.select("#order").on("change", function() {
+    clearTimeout(timeout)
+    var val = d3.select('#order :checked').node().value
+    order(val)
+  })
+
+/*
 
   function add_nodes(elem) {
     elem = elem.selectAll("g")
@@ -135,15 +358,34 @@ queue().defer(d3.csv, "../data-6.1,6.3.csv")
   relayout(window.innerWidth)
   show(d3.keys(render)[0])
 
+  */
+
 })
 
 
 // Utility functions
 
-function empty_matrix(n) {
+function constant_matrix(n, c) {
+  c = c || 0.0
   return d3.range(0,n).map( () => {
-    return d3.range(0,n).map( () => 0.0 )
+    return d3.range(0,n).map( () => c )
   })
+}
+
+function lift(fn) {
+  return function(a,b) {
+
+    var n = a.length,
+        c = constant_matrix(n)
+
+    var i=-1; while(i++<n-1) {
+      var j=-1; while(j++<n-1) {
+        c[i][j] = fn(a[i][j], b[i][j])
+      }
+    }
+
+    return c
+  }
 }
 
 function total(matrix, i) {
